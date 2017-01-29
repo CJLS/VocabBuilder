848ecdee4be95638928c4e7b3ca5b3c0e79db840
charles <jordan_li80@hotmail.com>
2017-01-29 14:28:46 -0500

    Added functionality to allow user to select the num of vocab to be reviewed with EditText input

--- app/src/main/java/charlesli/com/personalvocabbuilder/ui/ReviewDialog.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/ui/ReviewDialog.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.SimpleCursorAdapter;
@@ -28,6 +31,8 @@ import static charlesli.com.personalvocabbuilder.controller.Review.WORDTODEF;
 
 public class ReviewDialog extends CustomDialog {
 
+    private boolean textChangeFromUser = true;
+
     public ReviewDialog(Context context, VocabDbHelper dbHelper) {
         super(context);
 
@@ -42,23 +47,21 @@ public class ReviewDialog extends CustomDialog {
         final String[] reviewCategory = {firstCategoryInCategoryCursor};
 
         Cursor cursor = dbHelper.getVocabCursor(firstCategoryInCategoryCursor);
-        final Integer maxRow = cursor.getCount();
-        final int[] reviewNumOfWords = {maxRow};
+        final int[] reviewNumOfWords = {cursor.getCount()};
 
         LayoutInflater li = LayoutInflater.from(context);
         View promptsView = li.inflate(R.layout.alert_dialog_review, null);
 
-        final TextView numText = (TextView) promptsView.findViewById(R.id.numberText);
+        final EditText reviewNumET = (EditText) promptsView.findViewById(R.id.numberText);
         Spinner spinner = (Spinner) promptsView.findViewById(R.id.spinner);
         final RadioButton wordDef = (RadioButton) promptsView.findViewById(R.id.wordDef);
         final RadioButton defWord = (RadioButton) promptsView.findViewById(R.id.defWord);
         final SeekBar seekBar = (SeekBar) promptsView.findViewById(R.id.seekBar);
 
-        numText.setText(String.valueOf(maxRow));
-
-        setUpSpinner(categoryCursor, numText, spinner, seekBar, reviewCategory);
+        setUpReviewNumEditText(reviewNumOfWords, reviewNumET, seekBar);
+        setUpSpinner(categoryCursor, reviewNumET, spinner, seekBar, reviewCategory, reviewNumOfWords);
         setUpRadioButtons(reviewMode, wordDef, defWord);
-        setUpSeekBar(maxRow, reviewNumOfWords, numText, seekBar);
+        setUpSeekBar(reviewNumOfWords, reviewNumET, seekBar);
 
         setView(promptsView);
 
@@ -86,15 +89,49 @@ public class ReviewDialog extends CustomDialog {
 
     }
 
-    private void setUpSeekBar(Integer maxRow, final int[] reviewNumOfWords, final TextView numText, SeekBar seekBar) {
-        seekBar.setMax(maxRow);
-        seekBar.setProgress(maxRow);
+    private void setUpReviewNumEditText(final int[] reviewNumOfWords,
+                                        EditText reviewNum, final SeekBar seekBar) {
+        reviewNum.setText(String.valueOf(reviewNumOfWords[0]));
+
+        reviewNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (textChangeFromUser && !s.toString().equals("")) {
+                    int inputNum = Integer.parseInt(s.toString());
+                    if (inputNum > reviewNumOfWords[0]) {
+                        inputNum = reviewNumOfWords[0];
+                    }
+                    reviewNumOfWords[0] = inputNum;
+                    seekBar.setProgress(inputNum);
+                }
+            }
+        });
+    }
+
+    private void setUpSeekBar(final int[] reviewNumOfWords,
+                              final TextView numText, SeekBar seekBar) {
+        seekBar.setMax(reviewNumOfWords[0]);
+        seekBar.setProgress(reviewNumOfWords[0]);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                numText.setText(String.valueOf(progress));
-                reviewNumOfWords[0] = progress;
+                if (fromUser) {
+                    reviewNumOfWords[0] = progress;
+                    textChangeFromUser = false;
+                    numText.setText(String.valueOf(progress));
+                    textChangeFromUser = true;
+                }
             }
 
             @Override
@@ -129,7 +166,8 @@ public class ReviewDialog extends CustomDialog {
         });
     }
 
-    private void setUpSpinner(final Cursor categoryCursor, final TextView numText, Spinner spinner, final SeekBar seekBar, final String[] reviewCategory) {
+    private void setUpSpinner(final Cursor categoryCursor, final TextView numText, Spinner spinner,
+                              final SeekBar seekBar, final String[] reviewCategory, final int[] reviewNumOfWords) {
         String[] from = {VocabDbContract.COLUMN_NAME_CATEGORY};
         int[] to = {android.R.id.text1};
 
@@ -145,7 +183,10 @@ public class ReviewDialog extends CustomDialog {
                 VocabDbHelper dbHelper = VocabDbHelper.getDBHelper(getContext());
                 Cursor cursor = dbHelper.getVocabCursor(reviewCategory[0]);
                 Integer maxRow = cursor.getCount();
+                reviewNumOfWords[0] = maxRow;
+                textChangeFromUser = false;
                 numText.setText(String.valueOf(maxRow));
+                textChangeFromUser = true;
                 seekBar.setMax(maxRow);
                 seekBar.setProgress(maxRow);
             }
@@ -155,5 +196,6 @@ public class ReviewDialog extends CustomDialog {
 
             }
         });
+
     }
 }
