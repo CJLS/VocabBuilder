05d97db22307a140b6bb53e387f027a5ca891e47
charles <jordan_li80@hotmail.com>
2017-02-12 16:40:08 -0500

    Added button style and redesigned review result screen to include toolbar

--- app/src/main/java/charlesli/com/personalvocabbuilder/controller/ReviewResult.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/controller/ReviewResult.java
@@ -2,6 +2,7 @@ package charlesli.com.personalvocabbuilder.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,9 @@ public class ReviewResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_result);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.result_toolbar);
+        setSupportActionBar(myToolbar);
 
         Button continueButton = (Button) findViewById(R.id.continueButton);
         continueButton.setOnClickListener(new View.OnClickListener() {
