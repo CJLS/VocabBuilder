746ea93364d8ce7b1ba9ed8220add5e9288c989b
charles <jordan_li80@hotmail.com>
2017-03-18 09:34:23 -0400

    Set up ExportCursorAdapter

---
+++ app/src/main/java/charlesli/com/personalvocabbuilder/sqlDatabase/ExportCursorAdaptor.java
@@ -0,0 +1,62 @@
+package charlesli.com.personalvocabbuilder.sqlDatabase;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CursorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import charlesli.com.personalvocabbuilder.R;
+
+/**
+ * Created by charles on 2017-03-17.
+ */
+
+public class ExportCursorAdaptor extends CursorAdapter {
+
+    private List<Integer> selectedItemsPositions;
+
+    public ExportCursorAdaptor(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+
+        selectedItemsPositions = new ArrayList<>();
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category, parent, false);
+        CheckBox box = (CheckBox) view.findViewById(R.id.exportCheckbox);
+        box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                int position = (int) compoundButton.getTag();
+                if (b) {
+                    //check whether its already selected or not
+                    if (!selectedItemsPositions.contains(position))
+                        selectedItemsPositions.add(position);
+                } else {
+                    //remove position if unchecked checked item
+                    selectedItemsPositions.remove((Object) position);
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        CheckBox box = (CheckBox) view.findViewById(R.id.exportCheckbox);
+        box.setTag(cursor.getPosition());
+
+        if (selectedItemsPositions.contains(cursor.getPosition()))
+            box.setChecked(true);
+        else
+            box.setChecked(false);
+    }
+}
