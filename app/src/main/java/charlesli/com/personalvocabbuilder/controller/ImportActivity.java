34aaf4d8504c738f773f87914c1965277ae39f35
charles <jordan_li80@hotmail.com>
2017-05-07 14:14:02 -0400

    Set up import activity UI to be able to import into existing categories or specified category

--- app/src/main/java/charlesli/com/personalvocabbuilder/controller/ImportActivity.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/controller/ImportActivity.java
@@ -1,33 +1,64 @@
 package charlesli.com.personalvocabbuilder.controller;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 import charlesli.com.personalvocabbuilder.R;
+import charlesli.com.personalvocabbuilder.sqlDatabase.VocabDbHelper;
+import charlesli.com.personalvocabbuilder.ui.ImportDialog;
 
 public class ImportActivity extends AppCompatActivity {
+
+    private int GET_FILE_RESULT_CODE = 1;
+    private int IMPORT_OPTION_ORIGINAL_CATEGORIES = 0;
+    private int IMPORT_OPTION_SPECIFIED_CATEGORY = 1;
+    private int importOption = IMPORT_OPTION_ORIGINAL_CATEGORIES;
+    private TextView exportFileName;
+    private EditText importCategoryName;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import);
 
-        TextView exportFileName = (TextView) findViewById(R.id.exportFileName);
+        exportFileName = (TextView) findViewById(R.id.exportFileName);
+        importCategoryName = (EditText) findViewById(R.id.importCategory);
         Button selectFileButton = (Button) findViewById(R.id.selectFileButton);
         Button importButton = (Button) findViewById(R.id.importConfirmButton);
+        final RadioButton originalCategories = (RadioButton) findViewById(R.id.originalCategoriesRB);
+        final RadioButton specifiedCategory = (RadioButton) findViewById(R.id.specifiedCategoryRB);
         selectFileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("text/*");
+                startActivityForResult(intent, GET_FILE_RESULT_CODE);
             }
         });
         importButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (uri == null) {
+                    Toast.makeText(getApplicationContext(), "No export file has been selected", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    readExportFile(uri);
+                    finish();
+                }
             }
         });
         Button cancelButton = (Button) findViewById(R.id.importCancelButton);
@@ -37,5 +68,118 @@ public class ImportActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        originalCategories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                specifiedCategory.setChecked(false);
+                originalCategories.setChecked(true);
+                importOption = IMPORT_OPTION_ORIGINAL_CATEGORIES;
+                importCategoryName.setEnabled(false);
+            }
+        });
+        specifiedCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                originalCategories.setChecked(false);
+                specifiedCategory.setChecked(true);
+                importOption = IMPORT_OPTION_SPECIFIED_CATEGORY;
+                importCategoryName.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ImportDialog.GET_FILE_RESULT_CODE && resultCode == RESULT_OK) {
+            uri = data.getData();
+            File file= new File(uri.getPath());
+            exportFileName.setText(file.getName());
+        }
+    }
+
+    public void readExportFile(Uri uri) {
+        /*TODO:
+            1. Make sure export with large number of rows doesn't clog main thread
+                - make it an async task operation
+            2. Test when no permission initially
+            3. Check export file name patter and give appropriate warning
+        */
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new InputStreamReader(getContentResolver().openInputStream(uri)));
+            String line;
+            int count = 0;
+            // Skip first header line
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                Log.d("Test5:", line);
+                //"Vocab,Definition,Level,Category Name,Category Description"
+                // 1. Split line by , that's not preceded by /
+                String[] row = line.split("(?<!\\\\),");
+                if (row.length < 4) {
+                    continue;
+                }
+                // 2. Get each item from array
+                String vocab = row[0];
+                String definition = row[1];
+                String progress = row[2];
+                String categoryName = row[3];
+                String categoryDescription = "";
+                if (row.length > 4) {
+                    categoryDescription = row[4];
+                }
+
+                Log.d("Test: ", "Vocab: " + vocab + ", Definition: " + definition
+                        + ", Progress: " + progress + ", Category Name: " + categoryName
+                        + ", Category Description: " + categoryDescription);
+                // 3. Convert each item from /, to ,
+                vocab = vocab.replace("\\,", ",");
+                definition = definition.replace("\\,", ",");
+                int level;
+                switch (progress) {
+                    case "Difficult":
+                        level = ReviewSession.DIFFICULT;
+                        break;
+                    case "Familiar":
+                        level = ReviewSession.FAMILIAR;
+                        break;
+                    case "Easy":
+                        level = ReviewSession.EASY;
+                        break;
+                    case "Perfect":
+                        level = ReviewSession.PERFECT;
+                        break;
+                    default:
+                        level = ReviewSession.DIFFICULT;
+                        break;
+                }
+                categoryName = categoryName.replace("\\,", ",");
+                categoryDescription = categoryDescription.replace("\\,", ",");
+
+                if (importOption == IMPORT_OPTION_SPECIFIED_CATEGORY) {
+                    categoryName = importCategoryName.getText().toString();
+                    categoryDescription = "";
+                }
+
+                // 4. Insert into category and mvvocab db
+                VocabDbHelper dbHelper = VocabDbHelper.getDBHelper(this);
+                if (!dbHelper.checkIfCategoryExists(categoryName)) {
+                    dbHelper.insertCategory(categoryName, categoryDescription);
+                }
+                dbHelper.insertVocab(categoryName, vocab, definition, level);
+
+                count++;
+
+                // TODO: REMOVE COUNT
+                if (count > 10) {
+                    break;
+                }
+
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
