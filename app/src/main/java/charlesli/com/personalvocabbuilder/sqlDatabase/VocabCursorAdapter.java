002b985dd041febdc52d57318e8c17b3caf1e846
charles <jordan_li80@hotmail.com>
2017-05-28 22:06:00 -0400

    Modified itemVocab and VocabCursorAdaptor for English pronunciation feature

--- app/src/main/java/charlesli/com/personalvocabbuilder/sqlDatabase/VocabCursorAdapter.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/sqlDatabase/VocabCursorAdapter.java
@@ -2,6 +2,9 @@ package charlesli.com.personalvocabbuilder.sqlDatabase;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import charlesli.com.personalvocabbuilder.R;
 
@@ -27,12 +31,35 @@ public class VocabCursorAdapter extends CursorAdapter {
     private static final int PERFECT = 3;
 
     public List<Integer> selectedItemsPositions;
+    private TextToSpeech textToSpeech;
 
 
     public VocabCursorAdapter(Context context, Cursor c, int flags) {
         super(context, c, 0);
 
         selectedItemsPositions = new ArrayList<>();
+
+        textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        for (Locale locale : textToSpeech.getAvailableLanguages()) {
+                            Log.d("Tests:", "-------------------");
+                            Log.d("Test Country:", locale.getCountry());
+                            Log.d("Test Display Country:", locale.getDisplayCountry());
+                            Log.d("Test Language:", locale.getLanguage());
+                            Log.d("Test Display Language:", locale.getDisplayLanguage());
+                            Log.d("Test Display Name:", locale.getDisplayName());
+                            Log.d("Test Display Script:", locale.getDisplayScript());
+                            Log.d("Test Display Variant:", locale.getDisplayVariant());
+                        }
+                    }
+                }
+            }
+        }, "com.google.android.tts");
+
     }
 
     @Override
@@ -58,29 +85,29 @@ public class VocabCursorAdapter extends CursorAdapter {
 
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        TextView tvVocabName = (TextView) view.findViewById(R.id.vocabName);
-        TextView tvVocabDefinition = (TextView) view.findViewById(R.id.vocabDefinition);
-        ImageView tvVocabLevel = (ImageView) view.findViewById(R.id.vocabLevel);
+    public void bindView(View view, Context context, final Cursor cursor) {
+        final TextView vocabNameTV = (TextView) view.findViewById(R.id.vocabName);
+        TextView vocabDefinitionTV = (TextView) view.findViewById(R.id.vocabDefinition);
+        ImageView vocabLevel = (ImageView) view.findViewById(R.id.vocabLevel);
 
-        String vocab = cursor.getString(cursor.getColumnIndexOrThrow(VocabDbContract.COLUMN_NAME_VOCAB));
+        final String vocab = cursor.getString(cursor.getColumnIndexOrThrow(VocabDbContract.COLUMN_NAME_VOCAB));
         String definition = cursor.getString(cursor.getColumnIndexOrThrow(VocabDbContract.COLUMN_NAME_DEFINITION));
         int level = cursor.getInt(cursor.getColumnIndexOrThrow(VocabDbContract.COLUMN_NAME_LEVEL));
 
-        tvVocabName.setText(vocab);
-        tvVocabDefinition.setText(definition);
+        vocabNameTV.setText(vocab);
+        vocabDefinitionTV.setText(definition);
 
         if (level == DIFFICULT) {
-            tvVocabLevel.setImageResource(R.drawable.level_difficult_orange);
+            vocabLevel.setImageResource(R.drawable.level_difficult_orange);
         }
         else if (level == FAMILIAR) {
-            tvVocabLevel.setImageResource(R.drawable.level_familiar_orange);
+            vocabLevel.setImageResource(R.drawable.level_familiar_orange);
         }
         else if (level == EASY) {
-            tvVocabLevel.setImageResource(R.drawable.level_easy_orange);
+            vocabLevel.setImageResource(R.drawable.level_easy_orange);
         }
         else if (level == PERFECT) {
-            tvVocabLevel.setImageResource(R.drawable.level_perfect_orange);
+            vocabLevel.setImageResource(R.drawable.level_perfect_orange);
         }
 
         CheckBox box = (CheckBox) view.findViewById(R.id.editCheckbox);
@@ -90,5 +117,19 @@ public class VocabCursorAdapter extends CursorAdapter {
             box.setChecked(true);
         else
             box.setChecked(false);
+
+        ImageView speaker = (ImageView) view.findViewById(R.id.vocabSpeaker);
+        speaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 21) {
+                    textToSpeech.speak(vocab, TextToSpeech.QUEUE_FLUSH, null, "1");
+
+                }
+                else {
+                    textToSpeech.speak(vocab, TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        });
     }
 }
