d9783dc698beec07334b3924cbec2a03c7540b70
charles <jordan_li80@hotmail.com>
2017-06-10 17:25:01 -0400

    Added cancel button to speech settings and fixed bug so that speech setting only gets adjusted after pressing positive button

--- app/src/main/java/charlesli/com/personalvocabbuilder/ui/SpeechSettingsDialog.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/ui/SpeechSettingsDialog.java
@@ -23,30 +23,37 @@ import charlesli.com.personalvocabbuilder.R;
 
 public class SpeechSettingsDialog extends CustomDialog {
 
-    private TextToSpeech textToSpeech;
-    private HashMap<String, Locale> languageLocaleMapping;
-    private int defaultLangSelectionPos;
+    private int selectedPos;
 
-    public SpeechSettingsDialog(Context context, String category, TextToSpeech textToSpeech,
-                                HashMap<String, Locale> languageLocaleMapping,
-                                ArrayList<String> engineAvailableLanguages, int defaultLangSelectionPos) {
+    public SpeechSettingsDialog(Context context, final String category, final TextToSpeech textToSpeech,
+                                final HashMap<String, Locale> languageLocaleMapping,
+                                final ArrayList<String> engineAvailableLanguages, int defaultLangSelectionPos) {
         super(context);
 
         setTitle("Speech Settings");
         LayoutInflater li = LayoutInflater.from(context);
         View promptsView = li.inflate(R.layout.alert_dialog_speech_settings, null);
 
-        this.textToSpeech = textToSpeech;
-        this.languageLocaleMapping = languageLocaleMapping;
-        this.defaultLangSelectionPos = defaultLangSelectionPos;
-
+        final SharedPreferences sharedPreferences = context
+                .getSharedPreferences(context.getResources().getString(R.string.sharedPrefSpeechFile), Context.MODE_PRIVATE);
+        selectedPos = sharedPreferences.getInt(category, defaultLangSelectionPos);
 
         setupLanguageSelector((Spinner) promptsView.findViewById(R.id.vocabLanguageSpinner),
-                engineAvailableLanguages, category);
+                engineAvailableLanguages);
 
         setView(promptsView);
 
         setButton(BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(category, selectedPos);
+                editor.apply();
+                textToSpeech.setLanguage(languageLocaleMapping.get(engineAvailableLanguages.get(selectedPos)));
+            }
+        });
+
+        setButton(BUTTON_NEGATIVE, "CANCEL", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
@@ -55,7 +62,7 @@ public class SpeechSettingsDialog extends CustomDialog {
 
     }
 
-    private void setupLanguageSelector(Spinner spinner, final List<String> languages, final String category){
+    private void setupLanguageSelector(Spinner spinner, final List<String> languages){
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
                 android.R.layout.simple_spinner_item, languages);
@@ -63,18 +70,13 @@ public class SpeechSettingsDialog extends CustomDialog {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(arrayAdapter);
-        final SharedPreferences sharedPreferences = getContext()
-                .getSharedPreferences(getContext().getResources().getString(R.string.sharedPrefSpeechFile), Context.MODE_PRIVATE);
-        spinner.setSelection(sharedPreferences.getInt(category, defaultLangSelectionPos));
+        spinner.setSelection(selectedPos);
 
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putInt(category, position);
-                editor.apply();
-                textToSpeech.setLanguage(languageLocaleMapping.get(languages.get(position)));
+                selectedPos = position;
             }
 
             @Override
